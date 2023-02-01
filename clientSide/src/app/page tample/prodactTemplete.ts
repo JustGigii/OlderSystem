45a1f887a0dded74f04406be73280ae4d3face8d
omri gigi <gigiomri@gmail.com>
2023-02-01 16:55:44 +0200

    add conntion with profact

---
+++ clientSide/src/app/page tample/prodactTemplete.ts
@@ -0,0 +1,11 @@
+export interface prodact{
+    prodactId: number,
+    pordactName: string,
+    prodactImage: string,
+    typeSize: number
+}
+export interface UpDateProdact{
+    prodactId: number,
+    pordactName: string,
+    typeSize: number
+}
