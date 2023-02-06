ee830241ed0824febb6a469acc18adb38d9989b3
unknown <daniellamoran22@gmail.com>
2023-02-06 09:43:18 +0200

    catalog done but some problems, before trying to change product pop up

--- clientSide/src/app/feathers/popup-product/popup-product.component.ts
+++ clientSide/src/app/feathers/popup-product/popup-product.component.ts
@@ -1,5 +1,6 @@
 import { prodact } from './../../page tample/prodactTemplete';
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'popup-product',
@@ -9,9 +10,21 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 export class PopupProductComponent implements OnInit {
   @Input() currProduct?: prodact;
   @Output() onClose: EventEmitter<any> = new EventEmitter();
+  @Output() onAddToCart: EventEmitter<any> = new EventEmitter();
+  wantedSizes: Array<{size: number, quantity: number}> = new Array<{size: number, quantity: number}>;
+  sizes: Record<number, string[]> = {
+    1 : ["ג","ב","א"],
+    2 : [],
+    3 : ["מידשכלךש","מידת מכנסייםםםםםםם","מידת מכנסיים"],
+    4 : ["XS","S","M","L","XL"],
+    5 : ["ללא מידה"]
+  };
 
-
-  constructor() { }
+  constructor() {
+    for (let i = 36; i <= 50; i++) {
+      this.sizes["2"].push(String(i));
+    }
+  }
 
   ngOnInit(): void {
   }
@@ -20,5 +33,24 @@ export class PopupProductComponent implements OnInit {
     this.onClose.emit();
   }
 
-  //add each size to dictionary, 
+  addSize(){
+    this.wantedSizes.push({size: 0, quantity: 1});
+  }
+
+  addToCart(){
+    this.sortWantedSizes();
+    var addedProduct = {
+      "pordactName" : this.currProduct?.pordactName,
+      "prodactId" : this.currProduct?.prodactId,
+      "wantedSizes" : this.wantedSizes
+    }
+    this.onAddToCart.emit(addedProduct);
+  }
+
+  sortWantedSizes(){
+  }
+
+  onQuantityInput() {
+    console.log("invalid");
+  }
 }
--- clientSide/src/app/routing/app-routing.module.ts
+++ clientSide/src/app/routing/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from '../pages/home-page/home-page.component';
 import { ManagementComponent } from '../pages/management/management.component';
 import { CatalogPageComponent } from '../pages/catalog-page/catalog-page.component';
+import { CartPageComponent } from '../pages/cart-page/cart-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/homePage', pathMatch: 'full' },
   { path: 'homePage', component: HomePageComponent },
   { path: 'management', component: ManagementComponent },
-  { path: 'productsCatalog', component: CatalogPageComponent }
+  { path: 'productsCatalog', component: CatalogPageComponent },
+  { path: 'cartPage', component: CartPageComponent }
 ];
 
 @NgModule({
